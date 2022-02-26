1a7fedcea33e28e49875c49e192d8419ad7d5295
Emmanuel Iyanu <emmanueliyanu2012@gmail.com>
2022-02-26 09:54:27 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router , Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Router>
+      <ToastContainer />
+        {/* <Route path="/" exact component={Home} /> */}
+        {/* <Route path="/about"  component={About} />
+        <Route path="/contact"  component={Contact} />
+        <Route path="/service"  component={Services} />
+        <Route path="/terms-condition"  component={TermsCondition} />
+        <Route path="/products"  component={Product} />
+        <Route path="/privacy-policy"  component={PrivacyPolicy} />
+        <Route path="/book-a-demo"  component={BookDemo} /> */}
+      </Router>
     </div>
   );
 }
